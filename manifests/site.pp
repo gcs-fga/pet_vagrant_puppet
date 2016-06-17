03c822b576416b08296e8739ea395d8292786b66
tiagoassuncao <thiagoribeironiquel@hotmail.com>
2016-06-17 15:34:49 -0300

    Adding user and data base

--- manifests/site.pp
+++ manifests/site.pp
@@ -29,10 +29,37 @@ class user{
     shell      => '/bin/bash',
     home       => '/home/pet',
   }
-  exec { 'psql':
+
+  exec { 'psql create user':
     user => "postgres",
     command => "/usr/bin/createuser pet",
     path => "/home/pet",
     unless => "/usr/bin/psql postgres -tAc \"SELECT 1 FROM pg_roles WHERE rolname='pet'\" | /bin/grep -q 1"
+  }
+
+  exec { 'create db pet':
+    user => "postgres",
+    command => "/usr/bin/createdb -O pet pet",
+    path => "/home/pet",
+    unless => "/usr/bin/psql -lqt | /usr/bin/cut -d \| -f 1 | /bin/grep -qw pet",
+  }
+
+  exec { 'implements deb version':
+    user => "postgres",
+    command => "/usr/bin/psql pet < /usr/share/postgresql/9.4/contrib/debversion.sql",
+    path => "/home/pet",
+  }
+
+  service {"postgresql":
+    ensure => running,
+    enable => true,
+    hasrestart => true,
+  }
+
+  exec { "creating tables":
+    command => "/vagrant/pet-update -c -nc",
+    user => "pet",
+    unless => "/usr/bin/psql pet -tAc \"SELECT 1 FROM team\" | /bin/grep -q 1",
+    path => "/",
   }
 }
